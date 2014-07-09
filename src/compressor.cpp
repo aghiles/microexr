e792e40e1cc6ef41791afdc12b7f31a103c2d7ca
Daniel Dresser <dresserd@gmail.com>
2014-07-08 19:09:28 -0700

    Fixes for compression corruption

--- src/compressor.cpp
+++ src/compressor.cpp
@@ -14,6 +14,8 @@
 
 #include <stdlib.h>
 #include <zlib.h>
+#include <string.h>
+#include <stdio.h>
 
 /*
 	CompressData
@@ -23,21 +25,26 @@
 */
 int CompressData( char *inPtr, int inSize, char *outPtr)
 {
+	// Don't try to compress 0 sized arrays
+	if( inSize == 0 ) return 0;
+
 	char *_tmpBuffer = (char*) malloc( inSize );
 
 	char *t1 = _tmpBuffer;
 	char *t2 = _tmpBuffer + (inSize + 1) / 2;
 	const char *stop = inPtr + inSize;
 
+	char *inIterate = inPtr;
+
 	while (true)
 	{
-		if (inPtr < stop)
-			*(t1++) = *(inPtr++);
+		if (inIterate < stop)
+			*(t1++) = *(inIterate++);
 		else
 			break;
 
-		if (inPtr < stop)
-			*(t2++) = *(inPtr++);
+		if (inIterate < stop)
+			*(t2++) = *(inIterate++);
 		else
 			break;
 	}
@@ -62,5 +69,18 @@ int CompressData( char *inPtr, int inSize, char *outPtr)
 
 	free( _tmpBuffer );
 
-	return Z_OK == cstatus ? outSize : 0;
+	if( cstatus == Z_OK )
+	{
+		return outSize;
+	}
+
+	// A buffer error means that the data cannot be compressed ( the compressed version is larger than the original )
+	// In this case, OpenEXR dictates that we write the original data
+	// Any other error is unexpected.
+	if( cstatus != Z_BUF_ERROR )  printf( "UNEXPECTED ERROR WRITING DEEP EXR\n" );
+
+	memcpy( outPtr, inPtr, inSize );
+
+	return inSize;
 }
+
